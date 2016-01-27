f4982a239661f33413da18b837dfead18235bf16
Aydin Hassan <aydin@hotmail.co.uk>
2016-01-27 20:40:47 +0000

    Uninstall + extra check when installing

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,6 +8,7 @@ use Composer\Installer\PackageEvent;
 use Composer\Installer\PackageEvents;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use DomainException;
 
 /**
  * Class Plugin
@@ -21,11 +22,6 @@ class Plugin implements PluginInterface, EventSubscriberInterface
      * @var string
      */
     const PACKAGE_TYPE = 'php-school-workshop';
-
-    /**
-     * @var bool
-     */
-    private $ansiconInstalled = false;
 
     /**
      * Apply plugin modifications to composer
@@ -48,7 +44,10 @@ class Plugin implements PluginInterface, EventSubscriberInterface
             ],
             PackageEvents::POST_PACKAGE_UPDATE => [
                 ['install', 0]
-            ]
+            ],
+            PackageEvents::POST_PACKAGE_UNINSTALL => [
+                ['unInstall', 0]
+            ],
         );
     }
 
@@ -88,22 +87,79 @@ class Plugin implements PluginInterface, EventSubscriberInterface
             return;
         }
 
-        $binary = basename(array_values($binaries)[0]);
+        $binary         = basename(array_values($binaries)[0]);
         $binaryLocation = sprintf('%s/%s', $binLocation, $binary);
+        $target         = $this->getInstallLocation($binary);
 
 
-        $os = PHP_OS;
-        if (strpos($os, 'Darwin') === 0) {
-            //mac
-            return symlink($binaryLocation, sprintf('/usr/local/bin/%s', $binary));
+        //if target exists and is symlink then we just remove it
+        if (file_exists($target) && is_link($target)) {
+            unlink($target);
         }
 
-        if (strpos($os, 'WIN') === 0) {
-            //windows
+        return symlink($binaryLocation, $target);
+    }
+
+    /**
+     * @param PackageEvent $event
+     */
+    public function unInstall(PackageEvent $event)
+    {
+        if (static::PACKAGE_TYPE !== $event->getOperation()->getPackage()->getType()) {
             return;
         }
 
-        //linux
-        return symlink($binaryLocation, sprintf('/usr/local/bin/%s', $binary));
+        $this->unInstallBinary($event);
+    }
+
+    /**
+     * @param PackageEvent $event
+     * @return bool|void
+     */
+    public function unInstallBinary(PackageEvent $event)
+    {
+        $binLocation = $event
+            ->getComposer()
+            ->getConfig()
+            ->get('bin-dir');
+
+        $binaries = $event
+            ->getOperation()
+            ->getPackage()
+            ->getBinaries();
+
+        if (count($binaries) === 0) {
+            return;
+        }
+
+        $binary         = basename(array_values($binaries)[0]);
+        $target         = $this->getInstallLocation($binary);
+
+        //remove link if it exists
+        if (file_exists($target) && is_link($target)) {
+            unlink($target);
+        }
+    }
+
+
+    /**
+     * @param string $binary
+     * @return string
+     */
+    private function getInstallLocation($binary)
+    {
+        $os = PHP_OS;
+        if (strpos($os, 'Darwin') === 0) {
+            //mac
+            $target = sprintf('/usr/local/bin/%s', $binary);
+        } elseif (strpos($os, 'WIN') === 0) {
+            //windows
+            throw new DomainException(sprintf('Windows is not supported'));
+        } else {
+            //linux
+            $target = sprintf('/usr/local/bin/%s', $binary);
+        }
+
+        return $target;
     }
 }
